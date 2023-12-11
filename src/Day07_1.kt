fc18dc61edcf9cb6ef1661a2f906b424b23225ad
DESKTOP-CVC67T5\javah <richrx4@gmail.com>
2023-12-11 18:49:40 +0800

    Day 7 Part 1 Simplified.

--- src/Day07_1.kt
+++ src/Day07_1.kt
@@ -14,26 +14,24 @@ fun main() {
                 hand,
                 line.substring( 6 ).toInt(),
                 // 第一張牌的 index
-                cards.indexOf(hand[0]).let { firstIndex ->
-                    hand.groupingBy { card ->
-                        firstIndex - cards.indexOf( card )
-                    }.eachCount().let { diffCounts ->
-                        when ( diffCounts.size ) {
-                            // 五張牌都一樣
-                            1 -> HandType.FIVE_OF_A_KIND
-                            // 四張牌都一樣+一張不一樣的牌 or 三張牌一樣+二張牌一樣
-                            2 -> if ( diffCounts.containsValue( 4 ))
-                                HandType.FOUR_OF_A_KIND else HandType.FULL_HOUSE
-                            // 三張牌一樣+其它二張不一樣 or 二張牌一樣+二張牌一樣+一張不一樣的牌
-                            3 -> if ( diffCounts.containsValue( 3 ))
-                                HandType.THREE_OF_A_KIND else HandType.TWO_PAIR
-                            // 二張牌一樣+三張不一樣的牌
-                            4 -> HandType.ONE_PAIR
-                            // 五張牌都不一樣
-                            else -> HandType.HIGH_CARD
-                        } // when
-                    } // eachCount.let
-                } // indexOf.let
+                hand.groupingBy { card ->
+                    card
+                }.eachCount().let { diffCounts ->
+                    when ( diffCounts.size ) {
+                        // 五張牌都一樣
+                        1 -> HandType.FIVE_OF_A_KIND
+                        // 四張牌都一樣+一張不一樣的牌 or 三張牌一樣+二張牌一樣
+                        2 -> if ( diffCounts.containsValue( 4 ))
+                            HandType.FOUR_OF_A_KIND else HandType.FULL_HOUSE
+                        // 三張牌一樣+其它二張不一樣 or 二張牌一樣+二張牌一樣+一張不一樣的牌
+                        3 -> if ( diffCounts.containsValue( 3 ))
+                            HandType.THREE_OF_A_KIND else HandType.TWO_PAIR
+                        // 二張牌一樣+三張不一樣的牌
+                        4 -> HandType.ONE_PAIR
+                        // 五張牌都不一樣
+                        else -> HandType.HIGH_CARD
+                    } // when
+                } // eachCount.let
             ) // Triple constructor
         } // substring.let
     }.sortedWith { triple1, triple2 ->
@@ -45,7 +43,6 @@ fun main() {
             }
         else ( triple1.third.ordinal - triple2.third.ordinal )
     }.forEachIndexed { index, triple ->
-        println( triple )
         totalWinnings += triple.second * ( index + 1 )
     } // forEach
 
