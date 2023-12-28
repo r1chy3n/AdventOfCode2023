ec77e825e8e090092c4ee450fa364a62e79285b3
DESKTOP-CVC67T5\javah <richrx4@gmail.com>
2023-12-28 18:19:54 +0800

    a neat version of Day 12 Part 1

--- src/Day12_1.kt
+++ src/Day12_1.kt
@@ -1,65 +1,55 @@
 import java.io.File
 
-@Suppress("SpellCheckingInspection")
 fun main() {
-    val qMark = '?'
+    val radix2 = 2
+
+    val padZero = '0'
+
+    val comma = ","
+    val oneOrMore = "[.]+"
+    val pathname = "src/Day12.txt"
+    val space = " "
     val zeroOrMore = "[.]*"
+
     val conditions = arrayOf( ".", "#" )
 
     var sum = 0
 
-    File( "src/Day12.txt" ).forEachLine { line ->
-        // "???.### 1,1,3"
-        // => [0]="???.###"
-        //    [1]="1,1,3"
-        line.split( " " ).let { records ->
-            // [1]="1,1,3"
-            records[ 1 ].split( "," ).map { str ->
-                // [0]="#"
-                // [1]="#"
-                // [2]="###"
-                "#".repeat( str.toInt())
-            }.toMutableList().let { mutableBrokens ->
-                // [0]="[.]*"
-                // [1]="#"
-                // [2]="[.]+"
-                // [3]="#"
-                // [4]="[.]+"
-                // [5]="###"
-                // [6]="[.]*"
-                mutableBrokens.lastIndex.downTo( 1 ).forEach { index ->
-                    mutableBrokens.add( index, "[.]+" )
-                } // IntProgress.forEach
+    File( pathname ).forEachLine { line ->
+        val recordData = line.split( space )
 
-                mutableBrokens.add( 0, zeroOrMore )
-                mutableBrokens.add( zeroOrMore )
-                // "[.]*#[.]+#[.]+###[.]*"
-                mutableBrokens.joinToString( "" ).toRegex()
-            }.let { brokenRegex ->
-                // [0]="???.###"
-                "\\?".toRegex().findAll( records[ 0 ]).map { matchR ->
-                    // 找出 ? 所在位置
-                    matchR.range.first
-                }.toList().let { qMarkPos ->
-                    records[ 0 ].count { char ->
-                        char == qMark
-                    }.let { qMarkCount ->
-                        0.rangeTo( "1".repeat( qMarkCount ).toInt( 2 )).forEach { binInt ->
-                            // "000" -> "111" >>> "..." -> "###"
-                            var candidate = records[ 0 ]
+        val groupCounts = recordData[ 1 ].split( comma ).map { sCount ->
+            sCount.toInt()
+        } // split.map
 
-                            Integer.toBinaryString( binInt ).padStart( qMarkCount, '0' ).forEachIndexed { index, char ->
-                                candidate = candidate.replaceRange( qMarkPos[ index ], qMarkPos[ index ] + 1, conditions[ char.digitToInt()])
-                            }
+        val rexBroken = ( zeroOrMore + groupCounts.joinToString( oneOrMore ) { count ->
+            "#".repeat(count)
+        } + zeroOrMore ).toRegex()
 
-                            if ( brokenRegex.matches( candidate )) {
-                                sum++
-                            } // if
-                        } // IntRange.forEach
-                    } // count.let
-                } // toList.let
-            } // let.let
-        } // line.split.let
+        val qnPos = "\\?".toRegex().findAll( recordData[ 0 ]).map { matchResult ->
+            matchResult.range.first
+        }.toList()
+
+        0L.rangeTo( "1".repeat( qnPos.size ).toLong( radix2 )).filter { binaryLong ->
+            java.lang.Long.toBinaryString( binaryLong ).count { char ->
+                char == '1'
+            } + recordData[ 0 ].count { unknownChar ->
+                unknownChar == '#'
+            } == groupCounts.sum()
+        }.forEach { binaryLong ->
+            var candidate = recordData[ 0 ]
+
+            java.lang.Long.toBinaryString( binaryLong ).padStart( qnPos.size, padZero ).forEachIndexed { binaryIndex, binaryChar ->
+                candidate = candidate.replaceRange(
+                    IntRange( qnPos[ binaryIndex ], qnPos[ binaryIndex ]),
+                    conditions[ binaryChar.digitToInt()]
+                ) // candidate.replaceRange
+            } // padStart.forEachIndexed
+
+            if ( rexBroken.matches( candidate )) {
+                sum++
+            } // if
+        } // filter.forEach
     } // File.forEachLine
 
     println( sum )
